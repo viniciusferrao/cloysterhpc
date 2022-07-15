a1020ec0c13b8f2e6dfea3629dff5a5abd40d73a
lbgracioso <lbgracioso@outlook.com>
2022-07-15 14:11:44 -0300

    Created verification files and integrated euid verification
    Signed-off-by: lbgracioso <lbgracioso@outlook.com>

---
+++ src/verification.h
@@ -0,0 +1,17 @@
+@@
+//
+// Created by Lucas B. Gracioso on 15/07/22.
+//
+
+#ifndef CLOYSTERHPC_VERIFICATION_H_
+#define CLOYSTERHPC_VERIFICATION_H_
+
+namespace cloyster {
+
+    void checkEffectiveUserId();
+
+}
+
+
+#endif //CLOYSTERHPC_VERIFICATION_H_
+
