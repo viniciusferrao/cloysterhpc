52b77e931fc87bcdf180f7ffea46e7f4a0df9ad1
Vinícius Ferrão <vinicius@ferrao.net.br>
2022-07-23 01:34:37 -0300

    Trying to enable Doxygen and fix cosmetic issues

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,5 +1,5 @@
 function(enable_doxygen)
-  option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" OFF)
+  option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" ON)
   if(ENABLE_DOXYGEN)
     set(DOXYGEN_CALLER_GRAPH YES)
     set(DOXYGEN_CALL_GRAPH YES)
--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,10 @@
 // Globals definitions
 bool cloyster::dryRun = false;
 
-int main(int argc, char** argv)
+/**
+ * @brief The entrypoint.
+ */
+int main(const int argc, const char** argv)
 {
     // TODO: Parse command line options for log levels
 
