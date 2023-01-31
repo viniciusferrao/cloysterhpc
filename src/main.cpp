cbbcae30f77efc8c5223167e828eac098dc3ea4d
lbgracioso <contact@lbgracioso.net>
2023-01-31 15:05:58 -0300

    Implement new options to CLI11, add log level change via CLI
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- src/main.cpp
+++ src/main.cpp
@@ -19,64 +19,102 @@
 #endif
 
 // Globals definitions
-bool cloyster::dryRun = true;
+bool cloyster::dryRun = false;
 
 /**
  * @brief The entrypoint.
  */
 int main(int argc, const char** argv)
 {
-    CLI::App app{ productName };
-
+    CLI::App app { productName };
 
     bool showVersion = false;
-    app.add_flag("-v,--version", showVersion, "Show version information");
+    app.add_flag("-v, --version", showVersion, "Show version information");
 
     bool runAsRoot = false;
     app.add_flag("-r, --root", runAsRoot, "Run with root permissions");
 
-    bool dryRun = true;
-    app.add_flag("-d, --dryrun", dryRun, "Perform a dry run installation");
+    bool dryRun = false;
+    app.add_flag("-d, --dry", dryRun, "Perform a dry run installation");
 
     bool enableTUI = false;
     app.add_flag("-t, --tui", enableTUI, "Enable TUI");
 
-    std::optional<std::string> logLevelInput;
-    app.add_option("-l, --log-level", logLevelInput, (fmt::format("Available log levels: {}", fmt::join(magic_enum::enum_names<Log::Level>(), ", "))) );
+    bool enableCLI = false;
+    app.add_flag("-c, --cli", enableCLI, "Enable CLI");
 
-    CLI11_PARSE(app, argc, argv);
+    bool runAsDaemon = false;
+    app.add_flag("-D, --daemon", runAsDaemon, "Run as a daemon");
 
-    Log::Level logLevel = Log::Level::Info;
-    if (logLevelInput) {
-        if (std::regex_match(logLevelInput.value(), std::regex("^[0-9]+$")))
-            logLevel = magic_enum::enum_cast<Log::Level>(stoi(logLevelInput.value())).value();
+    std::string logLevelInput = fmt::format("{}", magic_enum::enum_name(Log::Level::Info));
+    constexpr std::size_t logLevels { magic_enum::enum_count<Log::Level>() };
+
+    const std::vector<std::string> logLevelVector = []() {
+        constexpr auto& logLevelNames { magic_enum::enum_names<Log::Level>() };
+        return std::vector<std::string> { logLevelNames.begin(),
+            logLevelNames.end() };
+    }();
+
+    app.add_option("-l, --log-level", logLevelInput,
+           [&logLevelVector]() {
+               std::string result { "Available log levels:" };
+
+               for (std::size_t i = 0; const auto& logLevel : logLevelVector) {
+                   result += fmt::format(" {} ({}),", logLevel, i++);
+               }
+
+               result.pop_back();
+               return result;
+           }())
+        ->check(CLI::IsMember(logLevelVector, CLI::ignore_case)
+            | CLI::Range(logLevels - 1))
+        // This is a hack to solve the autogen string:
+        // -l,--log-level TEXT:({Trace,Debug,Info,Warn,Error,Critical,Off})
+        // OR (INT in [0 - 6])
+        ->option_text(" ");
+
+    CLI11_PARSE(app, argc, argv)
+
+    Log::init([&logLevelInput]() {
+        if (std::regex_match(logLevelInput, std::regex("^[0-9]+$")))
+            return magic_enum::enum_cast<Log::Level>(stoi(logLevelInput))
+                .value();
         else
-            logLevel = magic_enum::enum_cast<Log::Level>(logLevelInput.value()).value();
-    }
-
-    try {
-        Log::init(logLevel);
-        LOG_INFO("{} Started", productName);
-    } catch (const std::exception& ex) {
-        fmt::print("Invalid log level: {}\n{}", logLevelInput.value(), ex.what());
-        return EXIT_FAILURE;
-    }
+            return magic_enum::enum_cast<Log::Level>(
+                logLevelInput, magic_enum::case_insensitive)
+                .value();
+    }());
 
 #ifndef NDEBUG
-    fmt::print("Log level set to: {}", magic_enum::enum_name<Log::Level>(logLevel));
+    fmt::print("Log level set to: {}\n", logLevelInput);
 #endif
+
+
+    LOG_INFO("{} Started", productName);
 
     if (showVersion) {
         fmt::print("{}: Version {}\n", productName, productVersion);
         return EXIT_SUCCESS;
     }
 
-    if(runAsRoot)
+    if (runAsRoot)
         cloyster::checkEffectiveUserId();
 
     if (dryRun) {
         fmt::print("Dry run enabled.\n");
         cloyster::dryRun = dryRun;
+    }
+
+    //@TODO implement CLI feature
+    if (enableCLI) {
+        fmt::print("CLI feature not implemented.\n");
+        return EXIT_FAILURE;
+    }
+
+    //@TODO implement run as daemon feature
+    if (runAsDaemon) {
+        fmt::print("Daemon feature not implemented.\n");
+        return EXIT_FAILURE;
     }
 
     auto model = std::make_unique<Cluster>();
@@ -85,6 +123,8 @@ int main(int argc, const char** argv)
         auto view = std::make_unique<Newt>();
         auto presenter = std::make_unique<PresenterInstall>(model, view);
     }
+
+
 
 
 #ifndef NDEBUG
