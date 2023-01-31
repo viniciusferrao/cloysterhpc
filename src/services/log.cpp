18179c73d985f8e65c8f0721934474cd0b086456
Vinícius Ferrão <vinicius@ferrao.net.br>
2023-01-31 01:53:29 -0300

    Minor changes on logging for better compat
    * Changed the order of enum class to be more friendly: 0 means disabled.
    * Added an overload to accept integer values as initializer.
    * Added using enum on log.cpp for better readability.
    
    Signed-off-by: Vinícius Ferrão <vinicius@ferrao.net.br>

--- src/services/log.cpp
+++ src/services/log.cpp
@@ -10,6 +10,8 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+void Log::init(std::size_t level) { init(static_cast<Level>(level)); }
+
 void Log::init(Level level)
 {
     const auto& pattern { "%^[%Y-%m-%d %H:%M:%S.%e] %v%$" };
@@ -29,33 +31,35 @@ void Log::init(Level level)
         productName, sinks.begin(), sinks.end());
 
     switch (level) {
-        case Log::Level::Trace:
-            logger->set_level(spdlog::level::trace);
-            logger->flush_on(spdlog::level::trace);
+        using enum Log::Level;
+
+        case Off:
+            logger->set_level(spdlog::level::off);
+            logger->flush_on(spdlog::level::off);
             break;
-        case Log::Level::Debug:
-            logger->set_level(spdlog::level::debug);
-            logger->flush_on(spdlog::level::debug);
-            break;
-        case Log::Level::Info:
-            logger->set_level(spdlog::level::info);
-            logger->flush_on(spdlog::level::info);
-            break;
-        case Log::Level::Warn:
-            logger->set_level(spdlog::level::warn);
-            logger->flush_on(spdlog::level::warn);
-            break;
-        case Log::Level::Error:
-            logger->set_level(spdlog::level::err);
-            logger->flush_on(spdlog::level::err);
-            break;
-        case Log::Level::Critical:
+        case Critical:
             logger->set_level(spdlog::level::critical);
             logger->flush_on(spdlog::level::critical);
             break;
-        case Log::Level::Off:
-            logger->set_level(spdlog::level::off);
-            logger->flush_on(spdlog::level::off);
+        case Error:
+            logger->set_level(spdlog::level::err);
+            logger->flush_on(spdlog::level::err);
+            break;
+        case Warn:
+            logger->set_level(spdlog::level::warn);
+            logger->flush_on(spdlog::level::warn);
+            break;
+        case Info:
+            logger->set_level(spdlog::level::info);
+            logger->flush_on(spdlog::level::info);
+            break;
+        case Debug:
+            logger->set_level(spdlog::level::debug);
+            logger->flush_on(spdlog::level::debug);
+            break;
+        case Trace:
+            logger->set_level(spdlog::level::trace);
+            logger->flush_on(spdlog::level::trace);
             break;
     }
 
