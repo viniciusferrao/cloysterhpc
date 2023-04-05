9a141f172623908896276b7a7f51a8a900794f34
lbgracioso <contact@lbgracioso.net>
2023-04-05 15:03:32 -0300

    Add doctest library, create network test base
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

---
+++ test/source/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lucas Gracioso <contact@lbgracioso.net> on 4/4/23.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+// This is all that is needed to compile a test-runner executable.
