59e1e6df5324123ed0ee3250cf59df9a4b029c09
lbgracioso <contact@lbgracioso.net>
2023-06-12 14:30:14 -0300

    Add beegfs, grafana, influxdata and oneAPI repos
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- src/repos.h
+++ src/repos.h
@@ -184,6 +184,219 @@ const repofile alma8_baseos = { "AlmaLinux-BaseOS", true,
             )PGPKEY" };
 }
 
+namespace addons {
+const repofile beegfs = { "beegfs-7.3.3-rhel8", true,
+    "BeeGFS 7.3.3 (rhel8)",
+    "https://mirror.versatushpc.com.br/beegfs/beegfs_7.3.3/dists/rhel8/", "",
+    true, "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-beegfs_7.3.3",
+    R"PGPKEY(-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQINBGIYhN4BEADkbNO8cXAZqCsySRarZSomLRzD5lSjM2j61I1484+rk2dvUdqf
+            4LnmeyHJUqR0imj1GGNc67+K4uO9U+CEuqbl5IiYxXRay41QyyGAyk707aEeIaaR
+            XdEz6E6GFZkU7GHYHAgWhI0gka0JKZP8cJp4BBJJTG68EQUmAU6ChvrYDxlp56FQ
+            02AN0bPLw3989YlW8mgk8PUtvy6KWSOgq3SwM/qQVzyWhJ9LRZTVKWYWYDuljOsb
+            Fwye1lb2bS72yCai5Kf54IbsNgLngwsXeLzu6dL1rAGANTvyrHe8OETmSIVmX3T1
+            6OWvFWpYBFBBgRRp71Pa+Zi7027PxsnL7nURiXRuuZpuEL+I9WfzixVmegG6H/gn
+            lmGKAPHwBuduoiOPMmvH3OZeYHhHNswlPdV1nZ5+w6oFGYxaTuCjHv5LN07evX/p
+            j16Y228xAZpMPWD+fI+2omcXXkST+nnZVZ2YQOpPdthACnjWub/fQ0ASR4IShW+6
+            P9ldJz47rt14w4CWkef4JNz43nnqrJCK1LySr+uXNDXtmj7pv92PHeGimy4Y6Q6b
+            IdJiEltSkZAelayrj440icIIK19qY7mOJvR6jrCf4jkglKhzDuFAMwP1Z7j1Fvfy
+            F0MbQukVnt4wsnTslq1voZHT3iMf8miCPQ3vM3zUX8s+xzTPQiSBS1pqUwARAQAB
+            tCpCZWVHRlMgTWFpbnRhaW5lcnMgPHN1cHBvcnRAdGhpbmtwYXJxLmNvbT6JAlQE
+            EwEKAD4WIQQpwcIARapRaElrVrtMQ5flOcZa1gUCYhiE3gIbAwUJBaOagAULCQgH
+            AgYVCgkICwIEFgIDAQIeAQIXgAAKCRBMQ5flOcZa1krLD/wMJkMdXSUFhVpmjmQU
+            hMqI2ChvAz0IytTMHvFuj3TSIM4eh9tPgEeEVInZ0f2UrYp8Hw1rnHgsNDwhFU9x
+            cefNzZy51r/NqVA2AF2pL3GIH3nIRO03Q2KpjpgrIuCNz9riwfw6SOjN+iopejKG
+            CcCSLh7ByTvBToTl534NcTo4PO5ZqI3OTCpjQ/ijHbG3ib0NWwtG9uJY0XcfYUrw
+            8tlo25sN2eo8Bjo2qXfPkIpbeyIqW6oyqS1y2ZAeLwb85ySbCRlOAYeW9ff5IQnB
+            Ra1kyPu93JU5Re3oKneqmqrt29ebSpwQ37urLydpTbk+ozy3Lmv6jg9qtbEniYnN
+            dTuZ0dhC5f2BXEthsuGVvDsIgVpLTQWV1JvhtEyn00xAvI5TOznkQtJ9JrtV6p+k
+            a2TjrwJ97gSt9YtvotWaTX+mcHpkTfyy6YUZkTkp3jeN+njJ4C5TOJgdm3W1snbZ
+            2OJ4OqYpTWphJloU8r5S0lskxBj6qfykh1He2rjLnalnvyhM5hBH+LhKTQK8by0N
+            tucddNFF0IMYLLUIphNtvNXUFXN/iLO7/znnm78mSTcxo1avZLsuRo8Ee2G/O2lu
+            t7YMp8C6WXujmH6sRooNRwO2RlHYcxjd2nuh5USoLrqAc8mWE8BQnkSueD0GHZhe
+            h5ZVY+AcRmYOmP3YN+wuXu9D6LkCDQRiGITeARAAtuWPE6wbLEObq86tS5OvOkkK
+            N/0XFXvfE1ry5l5SNlKKtfnGQwcFjbEV15tXQovoPefAe8s+LNjvWMVOXO3FcztG
+            Kg42MfsdeR6SRFosAp1yxcAB7I2B3Ezv8I5J06vzSJCRTk7GOsGX1PEYkmDEB6Ya
+            lxGuR23ZbX8aP9xNdWsIykU7XnWReRvSTgDrmKInZCR1XH9+wchextYD5sV4p/OV
+            Taj6S0hlRhyNHiGSjv9UuACb0xA2F/XfhSZvKTjuCWrpFpEOcqto5QAqVquFnh5+
+            NgGC7a2teZq4NOyG9F9lhqYHIIAbiaYvxd2c4O0eyYk2FUJTfeEpTu/IY02y2U0Y
+            /pBIdNTwZhq1ovxNfxYGS6fHF7oau8m5sK8QXI7RVdxYSy+gPMii8rebpHSaDOyp
+            Jr6kUwmuTPB9mS26pjdlwtWMi1LUzQk6re8f8z0Z+mYzQdQjrquD8htag2oa+D3/
+            6N7KuqIzTlubRtqX0l5pOJ/DK8rTSxPL/ImVTrDGMLNRYuwNGr9I4PWbWweThN+N
+            tb1qpEWnhsOH2SEjhwaq16PuDh0Ar5Q8pXnVt3Gqs1XekEcb9b0iiVaXjHZLp3hh
+            sGCCWGefJPrO44j6MJFTD/UfzytOaqYUVQM15yV3Q5VJGQeQ0zVrolXxI8aJyRX1
+            Lc5ZrqBzF0viB6vWfXkAEQEAAYkCPAQYAQoAJhYhBCnBwgBFqlFoSWtWu0xDl+U5
+            xlrWBQJiGITeAhsMBQkFo5qAAAoJEExDl+U5xlrWmUkP/R5tQv5lhdGFNXzSjxyC
+            7N1E5RS1KUdozD0okDRCxHJEnMfsUNfRjFhwos4EETwKYKWSZz4CREbiqFFdyfBS
+            ATI5xnCJlsEuNbqo/wLeS5M1eicc3qdfclAsZJRhWo22XHia6mQAVDxh9hKCWsR6
+            kwcnMhG820oVJM2IKXF4/wC33NzY7oVGPbMvdSEMq3STa0ywyt1iDcFuhlVTAURG
+            taRvcfpeD2/kJjs8hpO9v/EJgN2s3u+TFKFF2XcG2JYB86gg7HwlobqKlZ6Tw8dH
+            UCjNb+Qxe6oBC3YicxVQhYuGUqMyNKXwAQaQjkRXT8b9KcWTWm5ZyjGlmkoqhq7H
+            j6VQ9p7dpGgaLwC7MZVD8nTCMp4KeKss5yHy8lZvq8Hwx7i7OjT0AfO8rKh96WHV
+            sAYZ/xa56tCPk3xkI1d19pjNOcpYX1FAPONiMr+0oocvgypYr6uyCORu87kIi4Lu
+            YMUTl4kRd2qw0RrLHXAQpRPN89Rea7sSaD5YW/+h88G+o2OFbCBhwz11QgDMZJco
+            dAhFsduYj+cgkFtkeNcLDDEBNHhVuG/jQMSXMcq13xwFanO8L/v4QBhQdY4HpYNG
+            NVaSi2cTtrcSON9YOC9CzldSdtrWO8cCqSGaJUTdOdqyY5o4mBlkvG9TGOE1fCST
+            eAA803kyjtUmppWom/VgR4x/
+            =3yN5
+            -----END PGP PUBLIC KEY BLOCK-----
+            )PGPKEY" };
+
+const repofile grafana = { "grafana", true,
+    "grafana",
+    "https://mirror.versatushpc.com.br/grafana/repodata/", "",
+    true, "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-grafana",
+    R"PGPKEY(-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQGNBGO4aiUBDAC82zo3vUyQH3yTCabQ7ZpospBg/xXBbJWbQNksIbEP/+I12CjB
+            zac1QcMFd27MJlyXpsTqqSo1ZHOisNy0Tmyl/WlqMyoMeChg+LmIHLNbvAK0jPOX
+            1Pt2OykXJWN9Ru+ZZ4uQNgdKO5nXS6CZtK+McfhRwwghp+vlZFJgqP6aGR2A4cZ7
+            IJpUQIoT/8GY6Fdx5TStTJucVUXjSJ3VqafZe4c0WHrk5Yb0UptYPBj9brZkmC9F
+            Uz6BLX6eO0HGLdwvYzoenlN1sD/2dclUtxoKYmfKDgpcG1V4vOClYPgOZ7g6jvwU
+            +nW39VGwR7yzbEAmGxVcd93QNUjTaZMfO3xJFm1UG5JwC6VJcd7Wp3hNHJle/y62
+            lw0N2AATqJ7AV6PXKBPNebXvCB0LqkAiC/W//imeMCk9hfREmb5rhf1s83owpJaQ
+            gScEtJYIVgOqgGoFE8wkCkHFG1slneLykmGK2xAJ2Rk63MIAE4hL9WKLV624LMid
+            JqH3YIEA6pR+GlEAEQEAAbQmR3JhZmFuYSBMYWJzIDxlbmdpbmVlcmluZ0BncmFm
+            YW5hLmNvbT6JAdQEEwEIAD4WIQQOIuuI454SJ3p3YK6eQ5sQLPPAxgUCY7hqJQIb
+            AwUJA8JnAAULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRCeQ5sQLPPAxhXnDACu
+            6rtTbZsbHYaotiQ757UX+Yu+hXTDBQe74ahEqKAYLg2JKzYNx2Q7UovvVLJ3JZQ4
+            e2lezdj7NkeyuSuiq1C/A58fqRICqNh8vRCqOQ9+zfUy9DHwkCrLUVY+31MGLh3G
+            nXuNrb4AzC2PPNL+VoJhhYnXoFO6Ko6ftzmKeIVeuNp6YfM95gyfIupXGvmwefgx
+            fHIaq0MaeFhIf1RgcvPyMVIMCUoaHMeA5+Z2REjc9iopT4YVzn7ZmoG5vlXIo2gX
+            HGWFUQDTD3PW9cURVdaHAYcN0owl4o90jef14Md9xgTUIDx6soFhD3wXpiV5z/HC
+            7BZqe5mdpp0vDuQNRkqX/uALOBDdoh/r5mBjFxOzNeBHAtf8Fer9/w6g222sGUz/
+            I3BCBFBRUKEBaExvonIEFToVDM4nHTCW9vTgnPOLkgX8GBfF3cobmnJlKrX5gLKQ
+            MKs+9JtaRi8+RBb8hOCm3tGxW+o6GKwZ6BGYrsTzFHNfWV42EwXJUhbfQnK5K0S5
+            AY0EY7hqJQEMAO/jPuCVTthJR5JHFtzd/Sew59YJVIb8FgCPaZRKZwZ0rznMuZDf
+            HB6pDdHe5yy84Ig2pGundrxURkax5oRqQsTc6KWU27DPpyHx5yva1A7Sf55A0/i6
+            XLBd2IFabijChiYhVxD/CFOwMtkhjU5CLY67fZ6FRB20ByrlDSNrhVMJ5F8lxRNb
+            Kh14Jc4Hk4F2Mm1+VlNdrmFqSzPF9JcEvUYHSuzOHi14L1jS2ECdyakbYLHGiHhj
+            dxuTVlUTEZ9fZ73qRLRViUsy1fwMWTUBWwyO5Qpgbtps3+WefusuJycWnQDOZxxr
+            0/SGxTE3qNn5kWXCg56t0YFISlhGM2ImU+BdTY+p8AthibdhZCTYswoghkPGVXbu
+            DGR98tVaeG1hLHsL3yh17VbukSCliyurOleQt2AuG9kKieU8zcxsXvFASz2fJOiQ
+            T7ehyDMCK0rLSigA66pZ63PVy05NnH4P4MNRvCE03KthblDrMiF0BckB0fDxBbd8
+            17FEDGkunWKWmwARAQABiQG8BBgBCAAmFiEEDiLriOOeEid6d2CunkObECzzwMYF
+            AmO4aiUCGwwFCQPCZwAACgkQnkObECzzwMbAYAv+PWbRuO7McuaD8itXAtqW9o4F
+            o9PBMGXXJuWfN2UathyGuS6iZNCdIZMZgpOfuuk2ctFKeQHizM/hfUrguNGhvZX+
+            xSbuq8M+/dx+c2Lse7NDP0Q8Pw9UaDHcW6gTTLizq/CWhFpOD2IH2ywxY3IrAvzG
+            R4pDs+NodJgLCQPd1ez/lGk90mk/j17Yue2sD2fwJyqWqbHZJe8qgfvEtn+WPK33
+            84JN9DgDkcq7ThoLxU0Q7U3SempJGT98Yg2RWMAPj51DqtZOIVdeKoR8lr1rk3Kv
+            X7sojTBU4eWUrc0A3GwoqyCXz9xlXb8OLhTsFAlsQCLkgK7Rdt3sXyg3QkFQmGuk
+            MnYQV0TkaAcXE2p03nk45vVrWoGJPzDfx68LBT6Ck/Ytw8/QHm4zqjZBLH5cMdax
+            Fj8eP2CocfRC+Lqv0azQwyEVMkYSMKoFbhXmjiBZn9JxblndKnVbByA1/nMAa0Q7
+            HTJC50jDJfpM9d1xQW/W5LBSQjd3czM6zlRXsliX
+            =lSMJ
+            -----END PGP PUBLIC KEY BLOCK-----
+            )PGPKEY" };
+
+const repofile influxdata = { "influxdata", true,
+    "InfluxData Repository - Stable",
+    "https://mirror.versatushpc.com.br/influxdata/repodata/", "",
+    true, "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-influxdata",
+    R"PGPKEY(-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQINBGPIEycBEACpG4qSjhxA6fh4QJVJxFVBvCFt9tVx/hDbKH0Ryy9iilyMeReC
+            AS1/CZnSv/fhDNKmVPckf6on72z/ODwZcVfMV6DHkxmZ6x/tQrS6CWfKkupsON2H
+            KS3t4HUivahwHPlWtbfDqsWNwTAsZqklKpJQWY2ADPwurkbCmtYSjsgbLuWe23Pd
+            nJpLTHtlChM0ntW/l7Le1zYjGPUGoxMJgjg1YG8fi2l/zS0Of8bdQ26ps+WRvrSQ
+            RKhfAkfIgUiCXxBpDlN1spN73ZlAkaSb+myTfEKyJR55Yt9pHfkDdJh26RVgE1+N
+            GuLmm6oidaD9lTlNJ9P8wlLzoof3xJXYprgLLz/HmgtawnJ+DxFIXoXNNpUmhORJ
+            6Hb2Z5IKIyGIwXhQVe2Lw7B8awBNV99zUw517Wuax3RYx7Hwhntz9gFxS4GRxaCo
+            uLCFQ0AgDCkMHyEHufQo1XdjIB7fz6U551y5GMQw6/rjMnUM9ZI68SQ/FWou2cQf
+            533PyayvWOYQM4pP7ZmbzyCd393XlMaPWA5dyUOqv7Vcmv0IsAbncX6/KJmZAhKG
+            qu19xb6rv3ab2RbcU422guK3C/h/URPZJbSjf2w4jUV5UDe2veZg6BEVn7Sk5bW0
+            ceX8n0GVbPNG7CvRduJPjXNzsz3FzmUS8QFFde3H5gl1T0f6GcfhmKgKEQARAQAB
+            tDdJbmZsdXhEYXRhIFBhY2thZ2UgU2lnbmluZyBLZXkgPHN1cHBvcnRAaW5mbHV4
+            ZGF0YS5jb20+iQJVBBMBCAA/BgsJCAcDAgYVCAIJCgsEFgIDAQIeAQIXgAUJBaOk
+            /BYhBJ1TnZDTMo3H1sjTudj/jh99+LB+BQJjyB9PAhsDAAoJENj/jh99+LB+klgQ
+            AKOKdwTyKOr6+mnRrACz5U3EFxfAXXFGan9Ka7Nzgz4K+FOnTtT1gWwqrPPmTKQk
+            epNUMcelfX1kCA08yCm0nyw2niqxES40W33ergKUj6jlDx7UQYXWsDQGD9IKksa8
+            MWfZlJ3zlrsGKXA4oa+kfY+vltWDVP8WhLcQzm2LywbKvr3WgY80GZbnRjoekiBK
+            oMKztQVMJG5yNZBo9B4JrqB3wMpnXZxEtqZcBPsJJdXTFKHsQ7kB9TMNorbUvDNH
+            ohwsprgMw84vHikEk9jyCypXpYq/E/wvkM0CeIUJ36S2vGvACib7BiY6Xv0BQbM4
+            rWq2Rrjag1y5vVAF9gJkeo/3rhM6lE1ahDCRq0QcBMVzbxiE+3COIzRPmz14J3Yn
+            0pkvzlVkNj5UZR8q91ESl+UxkFCP1wzcXgs0dpJWirQIOZ9E2eYv3LcjE68xjW1k
+            c5q1GOGvJI7aXADxUZ4lFbz+NUb4Ts4HXHc8gV1Gm0vvmIqv2YfAvL5DXbKLdZxh
+            73CxKvBMmTXIEQ+vQJ3p1ZnUnb+l6DoxEFWg/hXHmE5jY3P6HIVFdliXF5FEs1lr
+            9snU2Pn1BDL+TBN7SX0QbKqArWA4qyn6eGH8Z1ULoUVBPCjwC9QuInp/9fqifFYo
+            OM3A51MDGyc/HCVG6jNJEI5h71QGHlPfyQybpjy7rQSe
+            =YwXc
+            -----END PGP PUBLIC KEY BLOCK-----
+            )PGPKEY" };
+
+const repofile oneAPI = { "oneAPI", true,
+    "Intel® oneAPI repository",
+    "https://mirror.versatushpc.com.br/oneAPI/repodata/", "",
+    true, "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-oneAPI",
+    R"PGPKEY(-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQENBFfriU0BCADD98BaTc+sNO2nk7UwfhNIwiyN+8PtoozyIQtITGViuLGTC2f7kcA08blM+pkz
+            qGc9+uYosY5zW++HPowg+b1IEvPAGv1/WzDgCcm8vMbvmgs9GQL64X81mkXglGO1xI7VFQ5KxvQY
+            oaOA4NFSw0L3RTYyGl2AAGRbkEmdUg+6DCUZ5aNp+ZUhH5sV3rpUwWj8aJw/1SUm8KmA6GTb8a+s
+            hQhygablAVyxY29/F4reIYD3TJsnKl9xcoc+NMaT76rOaL9TU71dhn9DRl6I2S63r4QmZXIgcikt
+            vzbWwny8IBPFrh/5HaVh+eGIVg7aC/6ZPvwLzVTiKtywP3JP9OXzABEBAAG1AEIiQ04gPSBJbnRl
+            bChSKSBTb2Z0d2FyZSBEZXZlbG9wbWVudCBQcm9kdWN0cyIsIE89SW50ZWwgQ29ycG9yYXRpb26J
+            AT4EEwEIACgFAlfriU0CGwMFCQWiSQAGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJEBqEl7EZ
+            EeCX1+gH/1ChlnCBN9SqB1xKW6gPPX+9UcqTD+kr1HPve64S0eSoV86U5iKNPVdJ3GcRrEd/hNuh
+            eiazQaxLjHBIkHMbur1x3/91uYFdVOndwKJwIRKZmP2vCjucNb8FO3I4lINevVn7IHTf4hMMyFEd
+            0ZLBvlmxXqOlV2Tb4ACGFPhLLhKNvkf6TFCCc0VoYmcyNecWEoh2ZZ9w/+uuY2pKkNulrjPFWjKA
+            G1TEIim+KkpzoBjBWAlb+Rf0OkahAWjc0LCbiA22oIdJxwsWYGqlJMSXCB5Q+Vk8duHRIm6tf5L3
+            7ymDS8lnPgq3871dhD+i5K6LTXdlY9Yk2Nv2BbDLSm42l+A=
+            =r6NW
+            -----END PGP PUBLIC KEY BLOCK-----
+            -----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQENBF2Ru1cBCADET6CERkWEDRAF6GIEceiajgMFT6BepRjF26rtnyEXdAXT4vU+wLqg0DE5Va1t
+            4ZAXcIHJ3g1OuGw7Vst2b7WySJl1cFvRSOxZg7Em0QUd51c7n0IYX6FL55Ern2pU3sL2NfJnyrKi
+            HBSTBGqMFELF5cTaY4E1GQyIUj0BrwUXMpLuxxL0ISyz2CNFnJuENbunbax0L5LrHLFwcsTEvT9q
+            ZVcP5G+xZBmhPOJ3bR3LHZN/9tj1E6IEzFOY/S/jfqXKmwbR4EzZ7pTtBqNzDm6bviWwQHyh6euw
+            eoDhTa6TAUUsP5vIL+7iWDQd9X10XyByWEsLaQbWNBCPxAwRTAgnABEBAAG1ACZJbnRlbChSKSBT
+            b2Z0d2FyZSBEZXZlbG9wbWVudCBQcm9kdWN0c4kBPgQTAQgAKAUCXZG7VwIbAwUJB4YfgAYLCQgH
+            AwIGFQgCCQoLBBYCAwECHgECF4AACgkQrPqfxX5sXb7L5Qf/cxJs9tDCea0ZWqeSX1lAjuff2Bku
+            H/yXwvu625DcAneUemho6NDck2LJKhpUM/9teyQWGTIIuz8IlIr3zYeK8VmVitm6cgjD07TYeh5j
+            71eAd7R1o49IUhIx/cXcuBcYHOw9kWCpl05D85xByZxkF7tlShLwVveyNGeVzU8aBJPhqoPGzIP0
+            /CJ3c2Qe09UMmqyhUw5ETMqBxR1E+vFCqAHfa3VdtOc8gIfmMK50t/wjE33RdnMI0cHoqmRLFj6C
+            ppBjBabIY+ro1focySyJ8cLkVEsk0T6u0s2B8nj+ctjasYAeZCmk6shNh9Y06zHkRcBS/cy2bHeB
+            4nKs3YBvpA==
+            =aXpT
+            -----END PGP PUBLIC KEY BLOCK-----
+            -----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQENBF07HV4BCADOChlmn/KbyUa5cffv4VlKfeI29FiOqXYW1AFhJLTmzvzTSTI6K1pdJKfZAT71
+            9uUz6p0lUkbjyTTgUOwQbXULyehGqe0dHQ8rVU1NPzWkmlu/Z1P7WWyd/z1gNRKXz0BcGE0HUfdh
+            IPSlNykqO1yaJfO55jT6KGjZHe4caR125my9EvwA9zAmF5MGQdNZPPnuh1VIEzqJAuPU/3hmBXjK
+            epsMO6Wz1IXCvFByAvsLLDHxRag1ImBzZ6zc2LQuGGcZfjuH0cnJ1moxKEbfl13eGTCCgKN2nVPj
+            b0G2uHY2xaWQMNBvYwuCWl4X7/iltJL21L0TNhECbiXgpvSS6Wd1ABEBAAG1ABVLRVktUElEVC1Q
+            R1AtMjAxOTA3MjaJAT4EEwEIACgFAl07HV4CGwMFCQeGH4AGCwkIBwMCBhUIAgkKCwQWAgMBAh4B
+            AheAAAoJEDa5Vps/GhvHMAUH/RwVG9JXvjLZ50nTo92Anm0cwdGq4Mb0Dr9eTg7xLxYiwV2TZ9Et
+            iSrjrmpvKXeboJBES8fFx4AtkOMq7t25b1HTq72UTeYhVaddjQ7saf0W/mxjvGIKJgJPtQKZuxDa
+            X5f4IQrhxbsR6rgXM4MOUR2ru+ejfZc6MQ7uQ1TKlA3/Z3aq1EXVzVCOzcg+N7Tz0VoqrRROuX0Z
+            +qFqK5QW3u/G9ORpSQq1C/ATI2+4qmSvFixoyx+FvIxqtozj5hXSG7cEaS8fGCdOjJhw1QXudfAr
+            izojEplMaapSKWoe3i0P+x5DdGBWuxLhA8iJt150rU4QMrh14FN7xLHBMY/7QW4=
+            =rudQ
+            -----END PGP PUBLIC KEY BLOCK-----
+            -----BEGIN PGP PUBLIC KEY BLOCK-----
+
+            mQENBF7Cku4BCADJYMbAmWE7IKY3QCIWm6EDSA4zvE2nj9mxOaTa02ZxXilEhQuU7pJlubqSbpPB
+            /+und2L0ITRnZgYCDNNH3702bFVZPjgy/6R4ruZpz0GjlwijHIwLSS57irkXjF6RHB8VFhNy7949
+            ua5a1NuBFvdRiaxSGE2mBv8pXrwiMqrS5mGXLVk6niS6zukVeQsxMZrdz1ScXvFeivotjbeTJu5K
+            S4fuPzUTieNwrB519V4zvs5WNZIT0WipQxh/AODJK5jFug7zdCtOFv4YOZ1dlSciO4hxmHo8hvb8
+            8mWHYmtBbZNWlpG1Aw6tmsKp3jVP/QvYaXZBdzoeEtxCCJBmxxxnABEBAAG1ADxDTj1JbnRlbChS
+            KSBTb2Z0d2FyZSBEZXZlbG9wbWVudCBQcm9kdWN0cyAoUFJFUFJPRCBVU0UgT05MWSmJAT4EEwEI
+            ACgFAl7Cku4CGwMFCQeGH4AGCwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJEGQHNkJ4cqIgc00H
+            /3aiK0HUaUh14TowUoKHxtD3tFNCRZ0BJIVmkxCKeKyqsFyUuIR7Ju36wgJjlRZgFaQo9eIgrTcq
+            AjpFbXQOoeKjSjwP4O2i8/N75Sb+FRemP2WVz78DQxBEJAMjOydc/J1QMtWcu2jVXmrrCxZBK7Nl
+            3TN/ZmQ5bgQ7Ifw1RFKtZCgGE50lRu2Zwcu4MKlIrIsovhds7HYCHhvG8VBEcoO5kqAVEeNr8iPX
+            1W/f+e8c3inRSwGHt4P9O/cAeW/OVGXllXKwpbVVmEVErfwGgJmR+9syBZZv9Kb6KWgeP/y9091E
+            wlB39FLnH/DsLu9AwNHGwDgmKTWQcwWhMQjnxRI=
+            =7ONs
+            -----END PGP PUBLIC KEY BLOCK-----
+            )PGPKEY" };
+
+    //@TODO ELRepo, rpmforge, Zabbix
+}
+
 class Repos {
 private:
     std::string m_filename;
