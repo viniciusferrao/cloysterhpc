705b05ed06f90389f912e3d428aa16fa00388285
lbgracioso <contact@lbgracioso.net>
2023-07-06 15:24:56 -0300

    Integrate inifile
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

---
+++ src/inifile.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Lucas Gracioso <contact@lbgracioso.net> on 7/6/23.
+//
+
+#include "inifile.h"
+
+void inifile::loadFile(const std::string& filepath)
+{
+    ini.LoadFile(filepath.c_str());
+}
+
+void inifile::loadFile(const std::string_view& filepath)
+{
+    ini.LoadFile(filepath.data());
+}
+
+void inifile::loadFile(const std::filesystem::path& filepath)
+{
+    ini.LoadFile(filepath.c_str());
+}
+
+std::string inifile::getValue(
+    const std::string& section, const std::string& key)
+{
+    return ini.GetValue(section.c_str(), key.c_str());
+}
+
+inifile::inifile() { ini.SetUnicode(); }
+
+void inifile::setValue(const std::string& section, const std::string& key,
+    const std::string& newValue)
+{
+    ini.SetValue(section.c_str(), key.c_str(), newValue.c_str());
+}
+
+bool inifile::deleteValue(const std::string& section, const std::string& key)
+{
+    return ini.Delete(section.c_str(), key.c_str());
+}
+void inifile::saveFile(const std::string& filepath)
+{
+    ini.SaveFile(filepath.c_str());
+}
+
+void inifile::saveFile(const std::string_view& filepath)
+{
+    ini.SaveFile(filepath.data());
+}
+
+void inifile::saveFile(const std::filesystem::path& filepath)
+{
+    ini.SaveFile(filepath.c_str());
+}
---
+++ src/inifile.h
@@ -0,0 +1,29 @@
+//
+// Created by Lucas Gracioso <contact@lbgracioso.net> on 7/6/23.
+//
+
+#ifndef CLOYSTERHPC_INIFILE_H_
+#define CLOYSTERHPC_INIFILE_H_
+
+#include "include/SimpleIni.h"
+#include <filesystem>
+
+class inifile {
+private:
+    CSimpleIniA ini;
+
+public:
+    void loadFile(const std::string& filepath);
+    void loadFile(const std::string_view& filepath);
+    void loadFile(const std::filesystem::path& filepath);
+    std::string getValue(const std::string& section, const std::string& key);
+    void setValue(const std::string& section, const std::string& key,
+        const std::string& newValue);
+    bool deleteValue(const std::string& section, const std::string& key);
+    void saveFile(const std::string& filepath);
+    void saveFile(const std::string_view& filepath);
+    void saveFile(const std::filesystem::path& filepath);
+    inifile();
+};
+
+#endif // CLOYSTERHPC_INIFILE_H_
