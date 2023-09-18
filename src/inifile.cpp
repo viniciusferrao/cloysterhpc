6c8300f180055bae48f01d34affb382fa41fe5ba
Vinícius Ferrão <vinicius@ferrao.net.br>
2023-09-18 02:16:12 -0300

    Fixed CMake testing and doctest
    Signed-off-by: Vinícius Ferrão <vinicius@ferrao.net.br>

--- src/inifile.cpp
+++ src/inifile.cpp
@@ -54,14 +54,71 @@ void inifile::saveFile(const std::filesystem::path& filepath)
     ini.SaveFile(filepath.c_str());
 }
 
+// BUG: Returning a pointer is not a good idea, it causes ownership issues.
 bool inifile::exists(const std::string& section, const std::string& key)
 {
     const auto* sample = ini.GetValue(section.c_str(), key.c_str());
     return sample != nullptr;
 }
 
+// BUG: Returning a pointer is not a good idea, it causes ownership issues.
 bool inifile::exists(const std::string& section)
 {
     const auto* sample = ini.GetSection(section.c_str());
     return sample != nullptr;
+}
+
+#ifdef BUILD_TESTING
+#include <doctest/doctest.h>
+#else
+#define DOCTEST_CONFIG_DISABLE
+#include <doctest/doctest.h>
+#endif
+
+TEST_SUITE("Load .ini files")
+{
+
+    TEST_CASE("Get information")
+    {
+        inifile ini;
+        std::filesystem::path path;
+        path = std::filesystem::current_path() / "sample/inifile.ini";
+        ini.loadFile(path);
+        const std::string clusterName
+            = ini.getValue("information", "cluster_name");
+        CHECK(clusterName == "cloyster");
+    }
+
+    TEST_CASE("Set value")
+    {
+        inifile ini;
+        std::filesystem::path path;
+        path = std::filesystem::current_path() / "sample/inifile.ini";
+        ini.loadFile(path);
+        const std::string newValue = "modified";
+        ini.setValue("information", "cluster_name", newValue);
+        CHECK(ini.getValue("information", "cluster_name") == newValue);
+    }
+
+    TEST_CASE("Delete value")
+    {
+        inifile ini;
+        std::filesystem::path path;
+        path = std::filesystem::current_path() / "sample/inifile.ini";
+        ini.loadFile(path);
+        const bool result = ini.deleteValue("information", "company_name");
+        CHECK(result);
+    }
+
+    TEST_CASE("Save to a new file")
+    {
+        inifile ini;
+        std::filesystem::path path;
+        std::filesystem::path newFile;
+        path = std::filesystem::current_path() / "sample/inifile.ini";
+        newFile = std::filesystem::current_path() / "sample/newinifile.ini";
+        ini.loadFile(path);
+        ini.saveFile(newFile);
+        CHECK(std::filesystem::exists(newFile));
+    }
 }
