a41f6e8e191404f64624d263b8dba7c2bf73122d
Lucas Gracioso <34951068+lbgracioso@users.noreply.github.com>
2023-11-13 13:47:08 -0300

    Change constexpr statements to use auto instead of char* (#29)
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- include/cloysterhpc/presenter/PresenterNodes.h
+++ include/cloysterhpc/presenter/PresenterNodes.h
@@ -11,49 +11,49 @@
 class PresenterNodes : public Presenter {
 private:
     struct Messages {
-        static constexpr const char* title = "Compute nodes settings";
-        static constexpr const char* message
+        static constexpr const auto title = "Compute nodes settings";
+        static constexpr const auto message
             = "We will now gather information to fill your compute nodes data";
 
         struct Nodes {
-            static constexpr const char* question
+            static constexpr const auto question
                 = "Enter the compute nodes information";
-            static constexpr const char* help
+            static constexpr const auto help
                 = Presenter::Messages::Placeholder::help;
 
-            static constexpr const char* prefix = "Prefix";
-            static constexpr const char* padding = "Padding";
-            static constexpr const char* startIP = "Compute node first IP";
-            static constexpr const char* rootPassword
+            static constexpr const auto prefix = "Prefix";
+            static constexpr const auto padding = "Padding";
+            static constexpr const auto startIP = "Compute node first IP";
+            static constexpr const auto rootPassword
                 = "Compute node root password";
-            static constexpr const char* confirmRootPassword
+            static constexpr const auto confirmRootPassword
                 = "Confirm compute node root password";
-            static constexpr const char* DiskImagePath
+            static constexpr const auto DiskImagePath
                 = "Disk image path of node OS";
         };
 
         struct Error {
-            static constexpr const char* prefixLetter
+            static constexpr const auto prefixLetter
                 = "Prefix must start with a letter";
-            static constexpr const char* paddingMax
+            static constexpr const auto paddingMax
                 = "We can only support up to 1000 nodes";
         };
 
         struct Quantity {
-            static constexpr const char* question
+            static constexpr const auto question
                 = "Enter the compute nodes quantity information";
-            static constexpr const char* help
+            static constexpr const auto help
                 = Presenter::Messages::Placeholder::help;
 
-            static constexpr const char* racks = "Racks";
-            static constexpr const char* nodes = "Nodes";
-            static constexpr const char* startNumber = "Node start number";
+            static constexpr const auto racks = "Racks";
+            static constexpr const auto nodes = "Nodes";
+            static constexpr const auto startNumber = "Node start number";
         };
 
         struct MAC {
-            static constexpr const char* question
+            static constexpr const auto question
                 = "Enter the MAC address for node";
-            static constexpr const char* help
+            static constexpr const auto help
                 = Presenter::Messages::Placeholder::help;
         };
     };
