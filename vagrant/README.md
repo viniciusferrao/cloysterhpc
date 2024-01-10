5753df5ef32258deafd47c8ef756974cf245e01a
lbgracioso <contact@lbgracioso.net>
2024-01-10 11:06:49 -0300

    Add Rocky support, create a extra disk to each machine
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- vagrant/README.md
+++ vagrant/README.md
@@ -7,7 +7,7 @@
 ### Dependencies
 
 ```shell
-$ dnf install -y dnf-plugins-core libvirt libvirt-devel qemu-kvm
+$ dnf install -y dnf-plugins-core libvirt libvirt-devel qemu-kvm virt-install
 
 $ dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 
@@ -42,12 +42,18 @@ $ chmod 644 /etc/pki/CA/cacert.pem
 
 ## Running
 
+**YOU MUST HAVE A `/mnt/iso` DIRECTORY IN YOUR HOST WITH ORIGINAL (DEFAULT) ISOs,
+SAME DISTRO AND VERSION AS YOUR DESIRED VAGRANT VMs. 
+DOWNLOAD IT FROM THE TABLE BELOW**
+
 Run `./setupVagrant /path/to/cloyster_binary 
 <vagrant_machine_name1> [<vagrant_machine_name2> ...] ` in this directory.
 
 You can SSH to the machine using `vagrant ssh <vagrant_machine_name>`.
 
-| Name   | Version |
-|--------|---------|
-| alma-8 | 8.x     |
+| Name     | Version | ISO                                                                                                                |
+|----------|---------|--------------------------------------------------------------------------------------------------------------------|
+| alma-8   | 8.9     | [AlmaLinux-8.9-x86_64-dvd.iso](https://repo.almalinux.org/almalinux/8.9/isos/x86_64/AlmaLinux-8.9-x86_64-dvd.iso ) |
+| rocky-8  | 8.9     | [Rocky-8.9-x86_64-dvd1.iso](https://download.rockylinux.org/pub/rocky/8/isos/x86_64/Rocky-8.9-x86_64-dvd1.iso)     |
+| rocky-9  | 9.3     | [Rocky-9.3-x86_64-dvd.iso](https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9.3-x86_64-dvd.iso)       |
 
--- vagrant/setupVagrant.sh
+++ vagrant/setupVagrant.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Cloyster Vagrant setup
 # Created by Lucas Gracioso <contact@lbgracioso.net>
 # SPDX-License-Identifier: Apache-2.0
@@ -25,12 +25,36 @@ fi
 
 MACHINES="$vagrant_machine_names_string" vagrant up
 
+## Loop through each Vagrant machine name, create a new disk and attach it to the VM
+for vagrant_machine_name in "${vagrant_machine_names[@]}"; do
+
+    # Create disk
+    qemu-img create -f raw /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img 50G # Create a new disk image
+
+    # Attach and configure
+    new_filesystem="vdb"
+    virsh attach-disk vagrant_"$vagrant_machine_name" /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img $new_filesystem --cache none --subdriver raw --persistent # Attach the new disk
+
+    vagrant ssh "$vagrant_machine_name" -c "sudo parted /dev/$new_filesystem mklabel msdos" # Create a new partition table
+    vagrant ssh "$vagrant_machine_name" -c "sudo parted /dev/$new_filesystem mkpart primary xfs 2048s 100%" # Create a new primary partition on the specified disk
+    vagrant ssh "$vagrant_machine_name" -c "sudo partprobe /dev/$new_filesystem" # Inform the kernel about the changes
+    vagrant ssh "$vagrant_machine_name" -c "sudo mkfs.xfs /dev/$new_filesystem\1" # Call mkfs.xfs to make the new filesystem
+    vagrant ssh "$vagrant_machine_name" -c "sudo mkdir /opt" # Make /opt dir if it doesn't exist
+    vagrant ssh "$vagrant_machine_name" -c "sudo mount /dev/$new_filesystem\1 /opt" # Mount the new filesystem to /opt
+    vagrant ssh "$vagrant_machine_name" -c "echo \"/dev/vdb1   /opt    xfs    defaults    0 0\" | sudo tee -a /etc/fstab" # Add to /etc/fstab
+
+    # Reload
+    vagrant reload "$vagrant_machine_name"
+done
+
 # Loop through each Vagrant machine name, copy the local file, SSH, run Cloyster and then destroy the machine
 for vagrant_machine_name in "${vagrant_machine_names[@]}"; do
     vagrant scp "$local_file_path" "$vagrant_machine_name":~/cloyster
     vagrant scp include/"$vagrant_machine_name".answerfile.ini "$vagrant_machine_name":~/answerfile.ini
     vagrant ssh "$vagrant_machine_name" -c "./cloyster -l 6 -a answerfile.ini"
 
-    # Comment if you don't want the VM to be destroyed right after running Cloyster
+    # Comment if you don't want the VM or the extra disk to be destroyed right after running Cloyster
     vagrant destroy "$vagrant_machine_name" -f
+    rm -f /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img
+
 done
