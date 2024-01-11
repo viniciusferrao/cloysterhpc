bccadf5b82a8672e67e294b5187b5f457d33c58b
lbgracioso <contact@lbgracioso.net>
2024-01-11 08:53:37 -0300

    Improve shellscript code
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- vagrant/setupVagrant.sh
+++ vagrant/setupVagrant.sh
@@ -15,46 +15,64 @@ shift  # Shift the first argument (local_file_path) off the argument list
 vagrant_machine_names=("$@")
 vagrant_machine_names_string=$(IFS=' '; echo "${vagrant_machine_names[*]}")
 
-# Check if the vagrant-scp plugin is installed
+# Check if vagrant-scp plugin is installed
 if ! vagrant plugin list | grep -q vagrant-scp; then
     echo "vagrant-scp plugin is not installed. Please install it using 'vagrant plugin install vagrant-scp'."
     exit 1
 fi
 
-# Run Vagrantfile
 
-MACHINES="$vagrant_machine_names_string" vagrant up
-
-## Loop through each Vagrant machine name, create a new disk and attach it to the VM
-for vagrant_machine_name in "${vagrant_machine_names[@]}"; do
+configure_disk() {
+    machine_name="$1"
+    new_filesystem="vdb"
+    disk_image="/var/lib/libvirt/images/${machine_name}_extra.img"
 
     # Create disk
-    qemu-img create -f raw /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img 50G # Create a new disk image
+    qemu-img create -f raw "$disk_image" 50G
 
     # Attach and configure
-    new_filesystem="vdb"
-    virsh attach-disk vagrant_"$vagrant_machine_name" /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img $new_filesystem --cache none --subdriver raw --persistent # Attach the new disk
+    virsh attach-disk "vagrant_$machine_name" "$disk_image" "$new_filesystem" --cache none --subdriver raw --persistent
 
-    vagrant ssh "$vagrant_machine_name" -c "sudo parted /dev/$new_filesystem mklabel msdos" # Create a new partition table
-    vagrant ssh "$vagrant_machine_name" -c "sudo parted /dev/$new_filesystem mkpart primary xfs 2048s 100%" # Create a new primary partition on the specified disk
-    vagrant ssh "$vagrant_machine_name" -c "sudo partprobe /dev/$new_filesystem" # Inform the kernel about the changes
-    vagrant ssh "$vagrant_machine_name" -c "sudo mkfs.xfs /dev/$new_filesystem\1" # Call mkfs.xfs to make the new filesystem
-    vagrant ssh "$vagrant_machine_name" -c "sudo mkdir /opt" # Make /opt dir if it doesn't exist
-    vagrant ssh "$vagrant_machine_name" -c "sudo mount /dev/$new_filesystem\1 /opt" # Mount the new filesystem to /opt
-    vagrant ssh "$vagrant_machine_name" -c "echo \"/dev/vdb1   /opt    xfs    defaults    0 0\" | sudo tee -a /etc/fstab" # Add to /etc/fstab
+    # Disk setup
+    vagrant ssh "$machine_name" -c "sudo parted /dev/$new_filesystem mklabel msdos"
+    vagrant ssh "$machine_name" -c "sudo parted /dev/$new_filesystem mkpart primary xfs 2048s 100%"
+    vagrant ssh "$machine_name" -c "sudo partprobe /dev/$new_filesystem"
+    vagrant ssh "$machine_name" -c "sudo mkfs.xfs /dev/${new_filesystem}1"
 
-    # Reload
+    # Mount and update /etc/fstab
+    vagrant ssh "$machine_name" -c "sudo mkdir /opt"
+    vagrant ssh "$machine_name" -c "sudo mount /dev/${new_filesystem}1 /opt"
+    vagrant ssh "$machine_name" -c "echo \"/dev/vdb1   /opt    xfs    defaults    0 0\" | sudo tee -a /etc/fstab"
+
+    # Reload machine
     vagrant reload "$vagrant_machine_name"
-done
+}
 
-# Loop through each Vagrant machine name, copy the local file, SSH, run Cloyster and then destroy the machine
+deploy_cloyster() {
+    machine_name="$1"
+    local_file_path="$2"
+
+    # Copy files to VM
+    vagrant scp "$local_file_path" "$machine_name":~/cloyster
+    vagrant scp include/"$machine_name".answerfile.ini "$machine_name":~/answerfile.ini
+
+    # Run Cloyster
+    vagrant ssh "$machine_name" -c "./cloyster -l 6 -a answerfile.ini"
+}
+
+destroy_vm_and_cleanup() {
+    machine_name="$1"
+
+    vagrant destroy "$machine_name" -f
+    rm -f "/var/lib/libvirt/images/${machine_name}_extra.img"
+}
+
+# Run Vagrantfile
+MACHINES="$vagrant_machine_names_string" vagrant up
+
+# Loop through each Vagrant machine and set up
 for vagrant_machine_name in "${vagrant_machine_names[@]}"; do
-    vagrant scp "$local_file_path" "$vagrant_machine_name":~/cloyster
-    vagrant scp include/"$vagrant_machine_name".answerfile.ini "$vagrant_machine_name":~/answerfile.ini
-    vagrant ssh "$vagrant_machine_name" -c "./cloyster -l 6 -a answerfile.ini"
-
-    # Comment if you don't want the VM or the extra disk to be destroyed right after running Cloyster
-    vagrant destroy "$vagrant_machine_name" -f
-    rm -f /var/lib/libvirt/images/"$vagrant_machine_name"_extra.img
-
+    configure_disk "$vagrant_machine_name"
+    deploy_cloyster "$vagrant_machine_name" "$local_file_path"
+    destroy_vm_and_cleanup "$vagrant_machine_name"
 done
