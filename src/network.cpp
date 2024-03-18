f1ed35836cdc8416ec820c9a8cfe3a8c2bf59092
lbgracioso <contact@lbgracioso.net>
2024-03-18 13:37:56 -0300

    Refactor network class tests and solve leak memory problem
    Signed-off-by: lbgracioso <contact@lbgracioso.net>

--- src/network.cpp
+++ src/network.cpp
@@ -182,12 +182,12 @@ address Network::fetchSubnetMask(const std::string& interface)
                 result.to_string(), interface);
 #endif
 
+            freeifaddrs(ifaddr);
             return result;
         }
     }
 
     freeifaddrs(ifaddr);
-    return {};
     throw std::runtime_error(fmt::format(
         "Interface {} does not have a netmask address defined", interface));
 }
@@ -262,12 +262,12 @@ address Network::fetchGateway(const std::string& interface)
                 result.to_string(), interface);
 #endif
 
+            freeifaddrs(ifaddr);
             return result;
         }
     }
 
     freeifaddrs(ifaddr);
-    return {};
     throw std::runtime_error(fmt::format(
         "Interface {} does not have a gateway IP address defined", interface));
 }
@@ -387,4 +387,97 @@ void Network::dumpNetwork() const
     LOG_DEBUG("Profile: {}", magic_enum::enum_name(m_profile));
     LOG_DEBUG("Type: {}", magic_enum::enum_name(m_type));
 }
+#endif
+
+#ifdef BUILD_TESTING
+#include <boost/asio.hpp>
+#include <cloysterhpc/tests.h>
+
+TEST_SUITE("Network setters and getters")
+{
+
+    auto testNetwork = Network(Network::Profile::External);
+
+    TEST_CASE("Get m_profile")
+    {
+        CHECK(testNetwork.getProfile() == Network::Profile::External);
+    }
+
+    TEST_CASE("Set and get m_address")
+    {
+        boost::asio::ip::address address
+            = boost::asio::ip::make_address("192.168.0.1");
+        testNetwork.setAddress(address);
+        CHECK(testNetwork.getAddress() == address);
+    }
+
+    TEST_CASE("Set and get m_subnetMask")
+    {
+        boost::asio::ip::address subnetMask
+            = boost::asio::ip::make_address("255.255.255.252");
+        testNetwork.setSubnetMask(subnetMask);
+        CHECK(testNetwork.getSubnetMask() == subnetMask);
+    }
+
+    TEST_CASE("Set and get m_gateway")
+    {
+        boost::asio::ip::address gateway
+            = boost::asio::ip::make_address("0.0.0.0");
+        testNetwork.setGateway(gateway);
+        CHECK(testNetwork.getGateway() == gateway);
+    }
+
+    TEST_CASE("Set and get m_vlan")
+    {
+        testNetwork.setVLAN(2000);
+        CHECK(testNetwork.getVLAN() == 2000);
+    }
+
+    TEST_CASE("Set and get m_domainName")
+    {
+        std::string domainName = "cloyster.com";
+        testNetwork.setDomainName(domainName);
+        CHECK(testNetwork.getDomainName() == domainName);
+    }
+
+    TEST_CASE("Set and get m_nameservers")
+    {
+        //@TODO must fix the setNameservers function
+    }
+}
+
+TEST_SUITE("Network fetchers")
+{
+
+    Network testNetwork;
+    auto testInterface = "enp0s25";
+
+    TEST_CASE("Fetch m_address")
+    {
+        //@TODO must fix the fetchAddress function
+    }
+
+    TEST_CASE("Fetch m_subnetMask")
+    {
+        CHECK_NOTHROW(testNetwork.fetchSubnetMask(testInterface));
+    }
+
+    TEST_CASE("Fetch m_gateway")
+    {
+        CHECK_NOTHROW(testNetwork.fetchGateway(testInterface));
+    }
+
+    TEST_CASE("Fetch m_domainName")
+    {
+        auto result = testNetwork.fetchDomainName();
+        CHECK_FALSE(result.empty());
+    }
+
+    TEST_CASE("Fetch m_nameservers")
+    {
+        auto result = testNetwork.fetchNameservers();
+        CHECK_FALSE(result.empty());
+    }
+}
+
 #endif
