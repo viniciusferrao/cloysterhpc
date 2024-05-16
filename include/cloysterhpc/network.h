c31c232e95c4bac5ec08da3eb5b0c21ed9ad4b7c
Lucas Gracioso <34951068+lbgracioso@users.noreply.github.com>
2024-05-16 07:16:30 -0300

    Implement Doxygen in (almost) all the headers
    Signed-off-by: Lucas Gracioso <34951068+lbgracioso@users.noreply.github.com>

--- include/cloysterhpc/network.h
+++ include/cloysterhpc/network.h
@@ -20,11 +20,31 @@ using boost::asio::ip::address;
  *  m_domainName is also available here since non-default networks may
  *  exist on the entire cluster and they should have their own domain.
  */
+/**
+ * @class Network
+ * @brief A class representing a network configuration.
+ *
+ * This class provides methods for configuring and retrieving network settings,
+ * such as IP addresses, subnet masks, gateways, VLANs, domain names, and name
+ * servers.
+ */
 class Network {
 public:
+    /**
+     * @enum Profile
+     * @brief Enum for network profiles.
+     */
     enum class Profile { External, Management, Service, Application };
+
+    /**
+     * @enum Type
+     * @brief Enum for network types.
+     */
     enum class Type { Ethernet, Infiniband };
 
+    /**
+     * @brief A mapping of subnet masks to their CIDR notation values.
+     */
     inline static const std::unordered_map<std::string, uint8_t> cidr
         = { // NOLINT
               { "0.0.0.0", 0 }, { "128.0.0.0", 1 }, { "192.0.0.0", 2 },
@@ -58,9 +78,35 @@ public:
     Network();
     explicit Network(Profile);
     Network(Profile, Type);
+
+    /**
+     * @brief Constructs a Network object with the specified parameters.
+     *
+     * @param profile The network profile.
+     * @param type The network type.
+     * @param ip The IP address.
+     * @param subnetMask The subnet mask.
+     * @param gateway The gateway address.
+     * @param vlan The VLAN ID.
+     * @param domainName The domain name.
+     * @param nameserver The list of name servers.
+     */
     Network(Profile, Type, const std::string& ip, const std::string& subnetMask,
         const std::string& gateway, const uint16_t& vlan,
         const std::string& domainName, const std::vector<address>& nameserver);
+
+    /**
+     * @brief Constructs a Network object with the specified parameters.
+     *
+     * @param profile The network profile.
+     * @param type The network type.
+     * @param ip The IP address.
+     * @param subnetMask The subnet mask.
+     * @param gateway The gateway address.
+     * @param vlan The VLAN ID.
+     * @param domainName The domain name.
+     * @param nameserver The list of name servers.
+     */
     Network(Profile, Type, const std::string& ip, const std::string& subnetMask,
         const std::string& gateway, const uint16_t& vlan,
         const std::string& domainName,
@@ -81,19 +127,54 @@ public:
     [[nodiscard]] address getAddress() const;
     void setAddress(const address& ip);
     void setAddress(const std::string& ip);
+
+    /**
+     * @brief Fetches the address associated with a network interface.
+     *
+     * @param interface The network interface.
+     * @return The address associated with the interface.
+     */
     [[nodiscard]] static address fetchAddress(const std::string& interface);
+
+    /**
+     * @brief Calculates the network address from a connection address.
+     *
+     * @param connectionAddress The connection address.
+     * @return The calculated network address.
+     */
     [[nodiscard]] address calculateAddress(const address& connectionAddress);
+
+    /**
+     * @brief Calculates the network address from a connection address.
+     *
+     * @param connectionAddress The connection address.
+     * @return The calculated network address.
+     */
     [[nodiscard]] address calculateAddress(
         const std::string& connectionAddress);
 
     [[nodiscard]] address getSubnetMask() const;
     void setSubnetMask(const address& subnetMask);
     void setSubnetMask(const std::string& subnetMask);
+
+    /**
+     * @brief Fetches the subnet mask associated with a network interface.
+     *
+     * @param interface The network interface.
+     * @return The subnet mask associated with the interface.
+     */
     [[nodiscard]] static address fetchSubnetMask(const std::string& interface);
 
     [[nodiscard]] address getGateway() const;
     void setGateway(const address& gateway);
     void setGateway(const std::string& gateway);
+
+    /**
+     * @brief Fetches the gateway address associated with a network interface.
+     *
+     * @param interface The network interface.
+     * @return The gateway address associated with the interface.
+     */
     [[nodiscard]] static address fetchGateway(const std::string& interface);
 
     [[nodiscard]] std::uint16_t getVLAN() const;
@@ -101,11 +182,23 @@ public:
 
     [[nodiscard]] const std::string& getDomainName() const;
     void setDomainName(const std::string& domainName);
+
+    /**
+     * @brief Fetches the domain name of the current network.
+     *
+     * @return The current network's domain name.
+     */
     [[nodiscard]] static std::string fetchDomainName();
 
     [[nodiscard]] std::vector<address> getNameservers() const;
     void setNameservers(const std::vector<address>& nameservers);
     void setNameservers(const std::vector<std::string>& nameservers);
+
+    /**
+     * @brief Fetches the list of name servers of the current network.
+     *
+     * @return The current network's list of name servers.
+     */
     [[nodiscard]] static std::vector<address> fetchNameservers();
 
 #ifndef NDEBUG
