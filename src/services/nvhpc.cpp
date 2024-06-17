b8d96c3e79b7a3af7c129e0814a42ce57762a4af
Lucas Gracioso <contact@lbgracioso.net>
2024-06-17 09:41:04 -0300

    Repo TODO warning
    Signed-off-by: Lucas Gracioso <contact@lbgracioso.net>

--- src/services/nvhpc.cpp
+++ src/services/nvhpc.cpp
@@ -6,6 +6,9 @@
 #include "cloysterhpc/services/nvhpc.h"
 #include "cloysterhpc/functions.h"
 
+//@TODO add repo
+//$ sudo dnf config-manager --add-repo
+//https://developer.download.nvidia.com/hpc-sdk/rhel/nvhpc.repo
 void NVhpc::install() { cloyster::runCommand("dnf -y install nvhpc-24.3"); }
 
 void NVhpc::enable() { }
