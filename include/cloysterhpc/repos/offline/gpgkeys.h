91098226fb380285f835578d2da6277e634afc88
Lucas Gracioso <contact@lbgracioso.net>
2024-06-18 08:00:48 -0300

    Change const types on gpgkeys.h
    Signed-off-by: Lucas Gracioso <contact@lbgracioso.net>

--- include/cloysterhpc/repos/offline/gpgkeys.h
+++ include/cloysterhpc/repos/offline/gpgkeys.h
@@ -2,105 +2,105 @@
 #include <string>
 
 // EL8 KEYS
-const std::string GPG_KEY_ALMA_8 =
+constexpr const char* GPG_KEY_ALMA_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-AlmaLinux"
     ;
 
-const std::string GPG_KEY_BEEGFS_8 =
+constexpr const char* GPG_KEY_BEEGFS_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-beegfs"
     ;
 
-const std::string GPG_KEY_ELREPO_8 =
+constexpr const char* GPG_KEY_ELREPO_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-elrepo"
     ;
 
-const std::string GPG_KEY_EPEL_8 =
+constexpr const char* GPG_KEY_EPEL_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-epel"
     ;
 
-const std::string GPG_KEY_GRAFANA_8 =
+constexpr const char* GPG_KEY_GRAFANA_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-grafana"
     ;
 
-const std::string GPG_KEY_INFLUXDATA_8 =
+constexpr const char* GPG_KEY_INFLUXDATA_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-influxdata"
     ;
 
-const std::string GPG_KEY_ONEAPI_8 =
+constexpr const char* GPG_KEY_ONEAPI_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-oneapi"
     ;
 
-const std::string GPG_KEY_OPENHPC_8 =
+constexpr const char* GPG_KEY_OPENHPC_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-openhpc"
     ;
 
-const std::string GPG_KEY_ORACLE_8 =
+constexpr const char* GPG_KEY_ORACLE_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-oracle"
     ;
 
-const std::string GPG_KEY_RHEL_8 =
+constexpr const char* GPG_KEY_RHEL_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-rhel-unavailable"
 
-    const std::string GPG_KEY_ROCKY_8 =
+    constexpr const char* GPG_KEY_ROCKY_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-rockyofficial"
     ;
 
-const std::string GPG_KEY_RPMFUSIONUPDATES_8 =
+constexpr const char* GPG_KEY_RPMFUSIONUPDATES_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-rpmfusion-updates"
     ;
 
-const std::string GPG_KEY_ZABBIX_8 =
+constexpr const char* GPG_KEY_ZABBIX_8 =
 #include "cloysterhpc/repos/offline/el8/RPM-GPG-KEY-zabbix"
     ;
 
 // EL9 KEYS
-const std::string GPG_KEY_ALMA_9 =
+constexpr const char* GPG_KEY_ALMA_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-AlmaLinux"
     ;
 
-const std::string GPG_KEY_BEEGFS_9 =
+constexpr const char* GPG_KEY_BEEGFS_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-beegfs"
     ;
 
-const std::string GPG_KEY_ELREPO_9 =
+constexpr const char* GPG_KEY_ELREPO_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-elrepo"
     ;
 
-const std::string GPG_KEY_EPEL_9 =
+constexpr const char* GPG_KEY_EPEL_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-epel"
     ;
 
-const std::string GPG_KEY_GRAFANA_9 =
+constexpr const char* GPG_KEY_GRAFANA_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-grafana"
     ;
 
-const std::string GPG_KEY_INFLUXDATA_9 =
+constexpr const char* GPG_KEY_INFLUXDATA_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-influxdata"
     ;
 
-const std::string GPG_KEY_ONEAPI_9 =
+constexpr const char* GPG_KEY_ONEAPI_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-oneapi"
     ;
 
-const std::string GPG_KEY_OPENHPC_9 =
+constexpr const char* GPG_KEY_OPENHPC_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-openhpc"
     ;
 
-const std::string GPG_KEY_ORACLE_9 =
+constexpr const char* GPG_KEY_ORACLE_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-oracle"
     ;
 
-const std::string GPG_KEY_RHEL_9 =
+constexpr const char* GPG_KEY_RHEL_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-rhel-unavailable"
 
-const std::string GPG_KEY_ROCKY_9 =
+    constexpr const char* GPG_KEY_ROCKY_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-rockyofficial"
     ;
 
-const std::string GPG_KEY_RPMFUSIONUPDATES_9 =
+constexpr const char* GPG_KEY_RPMFUSIONUPDATES_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-rpmfusion-updates"
     ;
 
-const std::string GPG_KEY_ZABBIX_9 =
+constexpr const char* GPG_KEY_ZABBIX_9 =
 #include "cloysterhpc/repos/offline/el9/RPM-GPG-KEY-zabbix"
     ;
