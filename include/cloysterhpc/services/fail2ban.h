ca2a8b9510939af9dfce434e341ac3a995b96457
Lucas Gracioso <contact@lbgracioso.net>
2024-07-02 22:30:57 -0300

    Change fail2ban function access modifiers
    Signed-off-by: Lucas Gracioso <contact@lbgracioso.net>

--- include/cloysterhpc/services/fail2ban.h
+++ include/cloysterhpc/services/fail2ban.h
@@ -6,11 +6,16 @@
 #ifndef CLOYSTERHPC_FAIL2BAN_H_
 #define CLOYSTERHPC_FAIL2BAN_H_
 
-#include "IService.h"
+#include <cloysterhpc/functions.h>
+#include <cloysterhpc/services/IService.h>
+#include <cloysterhpc/services/log.h>
+
 class fail2ban : public IService {
+private:
+    void configure() override;
+
 public:
     void install() override;
-    void configure() override;
     void enable() override;
     void disable() override;
     void start() override;
