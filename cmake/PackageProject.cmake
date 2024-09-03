c6209a65b83fbcca5393b187edd0ccb4214d4fac
Lucas Gracioso <contact@lbgracioso.net>
2024-09-03 15:27:39 -0300

    Small refactor on PackageProject.cmake
    Signed-off-by: Lucas Gracioso <contact@lbgracioso.net>

--- cmake/PackageProject.cmake
+++ cmake/PackageProject.cmake
@@ -1,5 +1,3 @@
-# Uses ycm (permissive BSD-3-Clause license) and ForwardArguments (permissive MIT license)
-
 function(cloysterhpc_package_project)
   cmake_policy(SET CMP0103 NEW) # disallow multiple calls with the same NAME
 
@@ -52,80 +50,8 @@ function(cloysterhpc_package_project)
   if("${_PackageProject_NAME}" STREQUAL "")
     set(_PackageProject_NAME ${PROJECT_NAME})
   endif()
-  # ycm args
-  set(_PackageProject_NAMESPACE "${_PackageProject_NAME}::")
-  set(_PackageProject_VARS_PREFIX ${_PackageProject_NAME})
+
   set(_PackageProject_EXPORT ${_PackageProject_NAME})
-
-  # default version to the project version
-  if("${_PackageProject_VERSION}" STREQUAL "")
-    set(_PackageProject_VERSION ${PROJECT_VERSION})
-  endif()
-
-  # default compatibility to SameMajorVersion
-  if("${_PackageProject_COMPATIBILITY}" STREQUAL "")
-    set(_PackageProject_COMPATIBILITY "SameMajorVersion")
-  endif()
-
-  # default to the build directory
-  if("${_PackageProject_CONFIG_EXPORT_DESTINATION}" STREQUAL "")
-    set(_PackageProject_CONFIG_EXPORT_DESTINATION "${CMAKE_BINARY_DIR}")
-  endif()
-  set(_PackageProject_EXPORT_DESTINATION "${_PackageProject_CONFIG_EXPORT_DESTINATION}")
-
-  # use datadir (works better with vcpkg, etc)
-  if("${_PackageProject_CONFIG_INSTALL_DESTINATION}" STREQUAL "")
-    set(_PackageProject_CONFIG_INSTALL_DESTINATION "${CMAKE_INSTALL_DATADIR}/${_PackageProject_NAME}")
-  endif()
-  # ycm args
-  set(_PackageProject_INSTALL_DESTINATION "${_PackageProject_CONFIG_INSTALL_DESTINATION}")
-
-  # Installation of the public/interface includes
-  if(NOT
-     "${_PackageProject_PUBLIC_INCLUDES}"
-     STREQUAL
-     "")
-    foreach(_INC ${_PackageProject_PUBLIC_INCLUDES})
-      # make include absolute
-      if(NOT IS_ABSOLUTE ${_INC})
-        set(_INC "${CMAKE_CURRENT_SOURCE_DIR}/${_INC}")
-      endif()
-      # install include
-      if(IS_DIRECTORY ${_INC})
-        # the include directories are directly installed to the install destination. If you want an `include` folder in the install destination, name your include directory as `include` (or install it manually using `install()` command).
-        install(DIRECTORY ${_INC} DESTINATION "./")
-      else()
-        install(FILES ${_INC} DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}")
-      endif()
-    endforeach()
-  endif()
-
-  # Append the configured public dependencies
-  if(NOT
-     "${_PackageProject_PUBLIC_DEPENDENCIES_CONFIGURED}"
-     STREQUAL
-     "")
-    set(_PUBLIC_DEPENDENCIES_CONFIG)
-    foreach(DEP ${_PackageProject_PUBLIC_DEPENDENCIES_CONFIGURED})
-      list(APPEND _PUBLIC_DEPENDENCIES_CONFIG "${DEP} CONFIG")
-    endforeach()
-  endif()
-  list(APPEND _PackageProject_PUBLIC_DEPENDENCIES ${_PUBLIC_DEPENDENCIES_CONFIG})
-  # ycm arg
-  set(_PackageProject_DEPENDENCIES ${_PackageProject_PUBLIC_DEPENDENCIES})
-
-  # Append the configured private dependencies
-  if(NOT
-     "${_PackageProject_PRIVATE_DEPENDENCIES_CONFIGURED}"
-     STREQUAL
-     "")
-    set(_PRIVATE_DEPENDENCIES_CONFIG)
-    foreach(DEP ${_PackageProject_PRIVATE_DEPENDENCIES_CONFIGURED})
-      list(APPEND _PRIVATE_DEPENDENCIES_CONFIG "${DEP} CONFIG")
-    endforeach()
-  endif()
-  # ycm arg
-  list(APPEND _PackageProject_PRIVATE_DEPENDENCIES ${_PRIVATE_DEPENDENCIES_CONFIG})
 
   # Installation of package (compatible with vcpkg, etc)
   set(CMAKE_INSTALL_PREFIX "/opt/${CMAKE_PROJECT_NAME}")
@@ -138,55 +64,5 @@ function(cloysterhpc_package_project)
     RUNTIME DESTINATION "${CMAKE_INSTALL_PREFIX}/sbin" COMPONENT bin
     PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${_PackageProject_NAME}" COMPONENT dev)
 
-  # install the usage file
-  set(_targets_str "")
-  foreach(_target ${_PackageProject_TARGETS})
-    set(_targets_str "${_targets_str} ${_PackageProject_NAMESPACE}${_target}")
-  endforeach()
-  set(USAGE_FILE_CONTENT
-      "The package ${_PackageProject_NAME} provides CMake targets:
-
-    find_package(${_PackageProject_NAME} CONFIG REQUIRED)
-    target_link_libraries(${cloysterhpc_BINARY_NAME} PRIVATE ${_targets_str})
-  ")
-  install(CODE "MESSAGE(STATUS \"${USAGE_FILE_CONTENT}\")")
-  file(WRITE "${_PackageProject_EXPORT_DESTINATION}/usage" "${USAGE_FILE_CONTENT}")
-  install(FILES "${_PackageProject_EXPORT_DESTINATION}/usage"
-          DESTINATION "${_PackageProject_CONFIG_INSTALL_DESTINATION}")
-
-  unset(_PackageProject_TARGETS)
-
-  # download ForwardArguments
-  FetchContent_Declare(
-    _fargs
-    URL https://github.com/polysquare/cmake-forward-arguments/archive/8c50d1f956172edb34e95efa52a2d5cb1f686ed2.zip)
-  FetchContent_GetProperties(_fargs)
-  if(NOT _fargs_POPULATED)
-    FetchContent_Populate(_fargs)
-  endif()
-  include("${_fargs_SOURCE_DIR}/ForwardArguments.cmake")
-
-  # prepare the forward arguments for ycm
-  set(_FARGS_LIST)
-  cmake_forward_arguments(
-    _PackageProject
-    _FARGS_LIST
-    OPTION_ARGS
-    "${_options};"
-    SINGLEVAR_ARGS
-    "${_oneValueArgs};EXPORT_DESTINATION;INSTALL_DESTINATION;NAMESPACE;VARS_PREFIX;EXPORT"
-    MULTIVAR_ARGS
-    "${_multiValueArgs};DEPENDENCIES;PRIVATE_DEPENDENCIES")
-
-  # download ycm
-  FetchContent_Declare(_ycm URL https://github.com/robotology/ycm/archive/refs/tags/v0.13.0.zip)
-  FetchContent_GetProperties(_ycm)
-  if(NOT _ycm_POPULATED)
-    FetchContent_Populate(_ycm)
-  endif()
-  include("${_ycm_SOURCE_DIR}/modules/InstallBasicPackageFiles.cmake")
-
-  install_basic_package_files(${_PackageProject_NAME} "${_FARGS_LIST}")
-
-  include("${_ycm_SOURCE_DIR}/modules/AddUninstallTarget.cmake")
+  include(cmake/CPackProperties.cmake)
 endfunction()
