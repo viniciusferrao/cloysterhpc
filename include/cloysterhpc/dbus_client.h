864479faf66f1904ac7cfdf01e3368edf7b128a0
Arthur Mendes <arthurmco@gmail.com>
2024-09-06 11:23:55 -0300

    Add DBUS client for postfix

---
+++ include/cloysterhpc/dbus_client.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <any>
+#include <memory>
+#include <sdbus-c++/sdbus-c++.h>
+#include <string>
+#include <type_traits>
+#include <variant>
+
+#include <cloysterhpc/messagebus.h>
+
+class DBusClient : public MessageBus {
+
+private:
+  std::unique_ptr<sdbus::IProxy> m_proxy;
+
+  //    std::unique_ptr<sdbus::IProxy>& getProxy();
+  sdbus::MethodCall createMethodCall(std::string interface, std::string method);
+
+public:
+  DBusClient(std::string bus, std::string object)
+      : m_proxy(
+            sdbus::createProxy(std::move(sdbus::createSystemBusConnection()),
+                               std::move(sdbus::ServiceName{bus}),
+                               std::move(sdbus::ObjectPath{object}))) {}
+
+  sdbus::MethodReply callMethod(const sdbus::MethodCall &message);
+
+  std::unique_ptr<MessageBusMethod> method(std::string interface,
+                                           std::string method);
+
+  virtual ~DBusClient() {}
+};
+
+class DBusMethod : public MessageBusMethod {
+  friend class DBusClient;
+
+private:
+  DBusClient *m_client;
+  sdbus::MethodCall m_message;
+
+  DBusMethod(DBusClient *c, sdbus::MethodCall &&message)
+      : m_client(c), m_message(message) {}
+  
+protected:
+  virtual void pushSingleParam(MethodParamVariant param);
+  virtual MessageReply callMethod();
+
+public:
+  DBusMethod() = delete;
+  
+};
