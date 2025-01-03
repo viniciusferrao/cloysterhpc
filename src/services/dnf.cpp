e97ebd13e4bd3041040a8b3d21673d37b913b431
Vinícius Ferrão <2031761+viniciusferrao@users.noreply.github.com>
2025-01-03 14:06:56 -0300

    Fixed formatting to `clang-format`.
    Signed-off-by: Vinícius Ferrão <2031761+viniciusferrao@users.noreply.github.com>

--- src/services/dnf.cpp
+++ src/services/dnf.cpp
@@ -1,44 +1,35 @@
 /*
-* Copyright 2025 Vinícius Ferrão <vinicius@ferrao.net.br>
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright 2025 Vinícius Ferrão <vinicius@ferrao.net.br>
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-#include <cloysterhpc/services/dnf.h>
 #include <cloysterhpc/functions.h>
+#include <cloysterhpc/services/dnf.h>
 #include <fmt/format.h>
 
 bool dnf::install(std::string_view package)
 {
-    return (cloyster::runCommand(fmt::format(
-               "dnf -y install {}", package)) != 0);
+    return (
+        cloyster::runCommand(fmt::format("dnf -y install {}", package)) != 0);
 }
 
 bool dnf::remove(std::string_view package)
 {
-    return (cloyster::runCommand(fmt::format(
-                "dnf -y remove {}", package)) != 0);
+    return (
+        cloyster::runCommand(fmt::format("dnf -y remove {}", package)) != 0);
 }
 
 bool dnf::update(std::string_view package)
 {
-    return (cloyster::runCommand(fmt::format(
-                "dnf -y update {}", package)) != 0);
+    return (
+        cloyster::runCommand(fmt::format("dnf -y update {}", package)) != 0);
 }
 
-bool dnf::update()
-{
-    return (cloyster::runCommand("dnf -y update") != 0);
-}
+bool dnf::update() { return (cloyster::runCommand("dnf -y update") != 0); }
 
-void dnf::check()
-{
-    cloyster::runCommand("dnf check");
-}
+void dnf::check() { cloyster::runCommand("dnf check"); }
 
-void dnf::clean()
-{
-    cloyster::runCommand("dnf clean all");
-}
+void dnf::clean() { cloyster::runCommand("dnf clean all"); }
 
 std::list<std::string> dnf::repolist()
 {
