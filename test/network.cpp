bce7a5aabbfbc5233bb8ba1a739a482c56e49fa3
Vinícius Ferrão <2031761+viniciusferrao@users.noreply.github.com>
2025-08-15 12:25:39 -0300

    Fix unreachable code in fetchSubnetMask

--- test/network.cpp
+++ test/network.cpp
@@ -7,8 +7,13 @@
 
 #include <boost/asio.hpp>
 #include <cloysterhpc/network.h>
+#include <stdexcept>
 
 TEST_SUITE("Network setters and getters")
 {
-
+    TEST_CASE("fetchSubnetMask throws for unknown interface")
+    {
+        CHECK_THROWS_AS(Network::fetchSubnetMask("nonexistent0"),
+            std::runtime_error);
+    }
 }
